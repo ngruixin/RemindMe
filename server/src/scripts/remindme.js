ea617e8583cb40d635ea3fc53e6b02a6ae1730a0
rn279 <rn279@cornell.edu>
2018-08-16 23:32:49 -0700

    init files

---
+++ server/src/scripts/remindme.js
@@ -0,0 +1,49 @@
+SERVER_IP = ""
+SERVER_PORT = ""
+
+$(document).ready(function() {
+	
+	$("#register").click(register);
+	$("#login").click(login)
+	
+});
+
+function register() {
+	var username = $("#username").val();
+	var password = $("#password").val();
+	var cfm_pwd = $("#cfm_pwd").val();
+	if (password != cfm_pwd) {
+		alert("Please make sure that the passwords match! ")
+		return;
+	}
+	var post_data = JSON.stringify({"username": username, "password": password});
+	if (post(post_data)) window.location = "login.html"; 
+	else alert("Registration failed, please try again.")
+}
+
+function login() {
+	var username = $("#username").val();
+	var password = $("#password").val();
+	var post_data = JSON.stringify({"username": username, "password": password});
+	if (post(post_data)) window.location = "reminder.html"; 
+	else alert("Login failed, please try again.")
+}
+
+function post(post_data) {
+	console.log(post_data);
+	return true;
+	// ajax?? 
+	/*$.ajax({
+	  type: 'POST',
+	  url: form.attr('action'),
+	  data: form.serialize(), // serializes form elements
+	  success: function(response) {
+	    // re-writes the entire document
+	    var newDoc = document.open("text/html", "replace");
+	    newDoc.write(response);
+	    newDoc.close();
+	  }
+	});*/
+}
+
+
